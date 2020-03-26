fc558d8be50440bbc8b74760ef61bcc71c584a24
guoziren <234917515@qq.com>
2020-03-26 23:17:38 +0800

    集成sdk

--- app/src/main/java/com/blts/himalaya/MainActivity.java
+++ app/src/main/java/com/blts/himalaya/MainActivity.java
@@ -2,12 +2,41 @@ package com.blts.himalaya;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList object) {
+                List<Category> categories = object.getCategories();
+                if (categories != null){
+                    Log.d(TAG, "onSuccess: " + categories.size());
+                    Log.d(TAG, "onSuccess: " + categories);
+                    for (Category category : categories) {
+                        Log.d(TAG, "onSuccess: category -> " + category.toString());
+                    }
+                }
+            }
+            @Override
+            public void onError(int code, String message) {
+            }
+        });
     }
 }
